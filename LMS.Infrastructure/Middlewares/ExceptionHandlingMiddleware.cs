8ebcadfff5a0103495b3035dffbf6683d26d8ca4
MahmoodElbadri <mahmoud.elbadry357@gmail.comgit config --global user.email mahmoud.elbadry357@gmail.comgit config --global user.email mahmoud.elbadry357@gmail.com>
2025-01-15 22:35:56 +0200

    Book Repository added

---
+++ LMS.Infrastructure/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace LMS.Infrastructure.Middlewares;
+
+public class ExceptionHandlingMiddleware
+{
+    private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+    private readonly RequestDelegate _next;
+
+    public ExceptionHandlingMiddleware(ILogger<ExceptionHandlingMiddleware> logger, RequestDelegate next)
+    {
+        this._logger = logger;
+        this._next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            //first thing to do is to log the exception
+            _logger.LogError(ex, ex.Message);
+            //second thing to do is to send a custom error response
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            context.Response.ContentType = "application/json";
+            var error = new
+            {
+                ErrorMessage = "An error occurred while processing your request.",
+                ExceptionMessage = ex.Message
+            };
+            await context.Response.WriteAsync(JsonSerializer.Serialize(error));
+        }
+    }
+}
---
+++ LMS.Infrastructure/Repositories/BooksRepository.cs
@@ -0,0 +1,120 @@
+﻿using LMS.Core.Interfaces.Repositories;
+using LMS.Core.Models;
+using LMS.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Infrastructure.Repositories;
+
+public class BooksRepository : IBookRepository
+{
+    private readonly ILogger<BooksRepository> _logger;
+    private readonly ApplicationDbContext _db;
+    public BooksRepository(ApplicationDbContext db, ILogger<BooksRepository> logger)
+    {
+        _logger = logger;
+        _db = db;
+    }
+
+
+    public async Task<Book> AddBookAsync(Book book)
+    {
+        try
+        {
+            await _db.Books.AddAsync(book);
+            await _db.SaveChangesAsync();
+            return book;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred while adding a book");
+            throw;
+        }
+    }
+
+    public async Task<Book> DeleteBookAsync(int ID)
+    {
+        try
+        {
+            var book = await _db.Books.FindAsync(ID);
+            if(book == null)
+            {
+                _logger.LogWarning($"Book with ID {ID} not found");
+                throw new Exception("Book not found");
+            }
+            else
+            {
+                _db.Books.Remove(book);
+                await _db.SaveChangesAsync();
+                return book;
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred while deleting a book");
+            throw;
+        }
+    }
+
+    public async Task<IEnumerable<Book>> GetAllBooksAsync()
+    {
+        try
+        {
+            return await _db.Books.ToListAsync();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred while getting all books");
+            throw;
+        }
+    }
+
+    public async Task<Book> GetBookByIDAsync(int ID)
+    {
+        try
+        {
+            //this is will not throw an exception to throw an exception in the controller 
+            //return await _db.Books.Where(tmp => tmp.ID == ID).FirstOrDefaultAsync();
+            return await _db.Books.FindAsync(ID);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred while getting a book by ID");
+            throw;
+        }
+    }
+
+    public async Task<Book> UpdateBookAsync(int ID, Book book)
+    {
+        try
+        {
+            var existBook = await _db.Books.FindAsync(ID);
+            if(book == null)
+            {
+                _logger.LogWarning($"Book with ID {ID} not found");
+                throw new Exception("Book not found");
+            }
+            else
+            {
+                existBook.Author = book.Author;
+                existBook.PublishDate = book.PublishDate;
+                existBook.Title = book.Title;
+                existBook.Loans = book.Loans;
+                existBook.IsBorrowed = book.IsBorrowed;
+                await _db.SaveChangesAsync();
+                return existBook;
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred while updating a book");
+            throw;
+        }
+        
+    }
+}
